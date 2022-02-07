256033fe0d29a51292f40f213b5e4601c5ec265d
Manfred Lee <manfredcml@gmail.com>
2022-02-07 19:42:14 +0800

    Renaming to avoid confusion

--- server/controllers/info.controller.ts
+++ server/controllers/info.controller.ts
@@ -13,10 +13,10 @@ const getRefs = async (req: Request, res: Response) => {
 
   switch (service) {
     case "git-upload-pack":
-      await handlePack("upload", repo, res)
+      await executeCmd("upload", repo, res)
       return
     case "git-receive-pack":
-      await handlePack("receive", repo, res)
+      await executeCmd("receive", repo, res)
       return
     default:
       res.status(400).send("Invalid service")
@@ -24,26 +24,24 @@ const getRefs = async (req: Request, res: Response) => {
   }
 }
 
-const handlePack = async (service: string, repo: string, res: Response) => {
+const executeCmd = async (service: string, repo: string, res: Response) => {
   const fullPath = getFullPath(repo)
   const cmd = `git-${service}-pack`
   const args = ["--stateless-rpc", "--advertise-refs", fullPath]
   const child = spawn(cmd, args)
-  let data = ""
-  for await (const chunk of child.stdout) {
-    const s = chunk.toString()
-    data += s
-    if (s.endsWith("0000")) {
-      break
-    }
-  }
+
   res.setHeader("Content-Type", `application/x-git-${service}-pack-advertisement`)
   res.setHeader("expires", "Fri, 01 Jan 1980 00:00:00 GMT")
   res.setHeader("pragma", "no-cache")
   res.setHeader("cache-control", "no-cache, max-age=0, must-revalidate")
+
   res.write(packSideband(`# service=git-${service}-pack` + "\n"))
   res.write("0000")
-  res.write(data)
+
+  for await (const chunk of child.stdout) {
+    res.write(chunk)
+  }
+
   res.end()
 }
 
